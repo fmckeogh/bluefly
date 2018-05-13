3be592fcfbbacafaeabc56b8185e51c3c9f29832
Ferdia McKeogh <chocol4te@users.noreply.github.com>
2018-05-13 16:58:44 +0100

    Trying a new signal design

--- src/display.rs
+++ src/display.rs
@@ -1,7 +1,7 @@
 use embedded_graphics::fonts::{Font, Font6x8};
 use embedded_graphics::prelude::Transform;
-use embedded_graphics::*;
 use embedded_graphics::primitives::Rect;
+use embedded_graphics::*;
 use hal::gpio::gpioa::{PA5, PA6, PA7};
 use hal::gpio::gpiob::PB1;
 use hal::gpio::{Alternate, Floating, Input, Output, PushPull};
@@ -49,7 +49,6 @@ pub fn write_display(disp: &mut OledDisplay, data: DisplayData) {
 
     disp.draw(Font6x8::render_str("TEST").translate((0, 50)).into_iter());
 
-
     // Remote battery
     disp.draw(Rect::new((111, 0), (126, 6), 1u8).into_iter());
     disp.set_pixel(127, 2, 1);
@@ -62,46 +61,32 @@ pub fn write_display(disp: &mut OledDisplay, data: DisplayData) {
         batt = ((0.12f32 * data.local_bat as f32) as u32 + 113) as u8;
     }
     for x in 113..batt {
-            disp.set_pixel(x.into(), 2, 1);
-            disp.set_pixel(x.into(), 3, 1);
-            disp.set_pixel(x.into(), 4, 1);
+        disp.set_pixel(x.into(), 2, 1);
+        disp.set_pixel(x.into(), 3, 1);
+        disp.set_pixel(x.into(), 4, 1);
     }
 
-    // Signal strength (TIDY UP AND MAKE IT LOOK BETTER)
-    if data.signal_strength > 20 {
-        for x in 0..2 {
-            for y in 6..7 {
-                disp.set_pixel(x, y, 1);
-            }
+    // Signal strength (TIDY UP AND REFACTOR)
+    for x in 0..3 { for y in 3..6 { disp.set_pixel(x, y, 1); }}
+    for x in 4..7 { for y in 2..6 { disp.set_pixel(x, y, 1); }}
+    for x in 8..11 {for y in 1..6 { disp.set_pixel(x, y, 1); }}
+    for x in 12..15 {for y in 0..6 { disp.set_pixel(x, y, 1); }}
+    if data.signal_strength < 80 {
+        for y in 1..5 {
+            disp.set_pixel(13, y, 0);
         }
     }
-    if data.signal_strength > 40 {
-        for x in 3..5 {
-            for y in 4..7 {
-                disp.set_pixel(x, y, 1);
-            }
+    if data.signal_strength < 60 {
+        for y in 2..5 {
+            disp.set_pixel(9, y, 0);
         }
     }
-    if data.signal_strength > 60 {
-        for x in 6..8 {
-            for y in 2..7 {
-                disp.set_pixel(x, y, 1);
-            }
+    if data.signal_strength < 40 {
+        for y in 3..5 {
+            disp.set_pixel(5, y, 0);
         }
     }
-    if data.signal_strength >= 80 {
-        for x in 9..11 {
-            for y in 0..7 {
-                disp.set_pixel(x, y, 1);
-            }
-        }
-    }
-    if data.signal_strength <= 20 {
-        // Make smaller font when available
-        disp.draw(
-            Font6x8::render_str("NO SIGNAL")
-                .translate((0, 0))
-                .into_iter(),
-        );
+    if data.signal_strength < 20 {
+        disp.set_pixel(1, 4, 0);
     }
 }
