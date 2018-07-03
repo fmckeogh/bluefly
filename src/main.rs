bfc9f2d7bfb89c4b5798901a4953a692a0c8acd2
Ferdia McKeogh <ferdia@mckeogh.tech>
2018-07-03 19:30:26 +0100

    Successfully writing magic bits to backup register

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-//! Blinks an LED
-
-//#![deny(unsafe_code)]
-//#![deny(warnings)]
 #![no_std]
 #![no_main]
 
-#[macro_use]
 extern crate cortex_m;
 #[macro_use]
 extern crate cortex_m_rt as rt;
@@ -15,16 +10,10 @@ extern crate stm32f103xx_hal as hal;
 #[macro_use(block)]
 extern crate nb;
 
-use core::fmt::Write;
-
-use core::ptr::read_volatile;
-use core::ptr::write_volatile;
-use cortex_m::asm;
 use hal::prelude::*;
 use hal::stm32f103xx;
 use hal::timer::Timer;
 use rt::ExceptionFrame;
-use sh::hio;
 
 entry!(main);
 
@@ -33,62 +22,34 @@ fn main() -> ! {
     let dp = stm32f103xx::Peripherals::take().unwrap();
 
     let mut flash = dp.FLASH.constrain();
-    let mut rcc1 = dp.RCC;
+    let mut rcc = dp.RCC;
     let mut bkp = dp.BKP;
     let mut pwr = dp.PWR;
 
-    let mut hstdout = hio::hstdout().unwrap();
-
-    writeln!(hstdout, "apb1enr: {:#034b}", rcc1.apb1enr.read().bits()).unwrap();
-    writeln!(hstdout, "cr: {:#034b}", pwr.cr.read().bits()).unwrap();
-
     // Enable backup registers
-    rcc1.apb1enr.modify(|_,w| w.bkpen().enabled());
-    rcc1.apb1enr.modify(|_,w| w.pwren().enabled());
-    pwr.cr.modify(|_,w| w.dbp().bit(true));
+    rcc.apb1enr.modify(|_, w| w.bkpen().enabled());
+    rcc.apb1enr.modify(|_, w| w.pwren().enabled());
+    pwr.cr.modify(|_, w| w.dbp().bit(true));
 
-    writeln!(hstdout, "apb1enr: {:#034b}", rcc1.apb1enr.read().bits()).unwrap();
-    writeln!(hstdout, "cr: {:#034b}", pwr.cr.read().bits()).unwrap();
+    let mut rcc_c = rcc.constrain();
 
-    let mut rcc = rcc1.constrain();
-
-    // Try a different clock configuration
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-    // let clocks = rcc.cfgr
-    //     .sysclk(64.mhz())
-    //     .pclk1(32.mhz())
-    //     .freeze(&mut flash.acr);
-
-    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+    let mut gpioc = dp.GPIOC.split(&mut rcc_c.apb2);
+    let clocks = rcc_c.cfgr.freeze(&mut flash.acr);
 
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-    // Try a different timer (even SYST)
     let mut timer = Timer::syst(cp.SYST, 1.hz(), clocks);
 
-    /*
-    unsafe {
-        write_volatile(0x40006C04 as *mut u32, 0x4F42u32);
-        write_volatile(0x40006C08 as *mut u32, 0x544Fu32);
-    }
-    */
-    bkp.dr1.write(|w| unsafe { w.bits(0x4F42u32) });
-    bkp.dr2.write(|w| unsafe { w.bits(0x544Fu32) });
-
-    /*
-    let upper: u32 = unsafe { read_volatile(0x40006C04 as *mut u32) };
-    let lower: u32 = unsafe { read_volatile(0x40006C08 as *mut u32) };
-    */
-
-    let upper: u32 = bkp.dr1.read().bits();
-    let lower: u32 = bkp.dr2.read().bits();
-
-    writeln!(hstdout, "upper: {:#X?}", upper).unwrap();
-    writeln!(hstdout, "lower: {:#X?}", lower).unwrap();
-
-    //iprintln!(&itm.stim[0], "upper: {}", upper);
-    //iprintln!(&itm.stim[0], "lower: {}", lower);
+    let mut count: u64 = 0;
 
     loop {
+        if count > 4 {
+            bkp.dr1.write(|w| unsafe { w.bits(0x4F42u32) });
+            bkp.dr2.write(|w| unsafe { w.bits(0x544Fu32) });
+            led.set_low();
+            while true {}
+        }
+        count += 1;
+
         block!(timer.wait()).unwrap();
         led.set_high();
         block!(timer.wait()).unwrap();
