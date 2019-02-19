b8324ea98c5de8f69b3ef92074df71ba4d0a8a7c
Ferdia McKeogh <f.mckeogh@1aim.com>
2019-02-19 18:46:34 +0100

    feat: rtfm on nrf modules

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -8,61 +8,29 @@ extern crate alloc;
 extern crate panic_semihosting;
 
 use alloc_cortex_m::CortexMHeap;
-use common::Request;
 use cortex_m_semihosting::hprintln;
 use embedded_graphics::{coord::Coord, fonts::Font6x8, prelude::*};
-use embedded_hal::spi::{Mode, Phase, Polarity};
-use nb::block;
+use nrf52810_hal::{
+    self as hal,
+    gpio::{Level, Output, Pin, PushPull},
+    nrf52810_pac::{self as device, SPIM0},
+    prelude::{GpioExt, SpimExt, _embedded_hal_digital_OutputPin},
+    Spim,
+};
 use rtfm::app;
-use ssd1306::{
-    interface::spi::SpiInterface, mode::graphics::GraphicsMode, prelude::DisplaySize, Builder,
-};
-use stm32f103xx_hal::{
-    gpio::{
-        gpioa::PA9,
-        gpiob::{PB13, PB14, PB15},
-        gpioc::PC13,
-        Alternate, Floating, Input, Output, PushPull,
-    },
-    prelude::*,
-    serial::Serial,
-    spi::Spi,
-    stm32f103xx::{self as device, ADC1, SPI2, USART1},
-};
-
-type Display = GraphicsMode<
-    SpiInterface<
-        Spi<
-            SPI2,
-            (
-                PB13<Alternate<PushPull>>,
-                PB14<Input<Floating>>,
-                PB15<Alternate<PushPull>>,
-            ),
-        >,
-        PA9<Output<PushPull>>,
-    >,
->;
+use ssd1306::{interface::spi::SpiInterface, mode::graphics::GraphicsMode, prelude::*, Builder};
 
 #[global_allocator]
 static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
 
-const PERIOD: u32 = 500_000;
-
-#[app(device = stm32f103xx_hal::stm32f103xx)]
+#[app(device = nrf52810_hal::nrf52810_pac)]
 const APP: () = {
-    static mut LED: PC13<Output<PushPull>> = ();
+    static mut COUNT: u64 = ();
+    static mut DISPLAY: GraphicsMode<SpiInterface<Spim<SPIM0>, Pin<Output<PushPull>>>> = ();
 
-    static mut ADC1: ADC1 = ();
-    static mut RADIO: (
-        stm32f103xx_hal::serial::Tx<USART1>,
-        stm32f103xx_hal::serial::Rx<USART1>,
-    ) = ();
-    static mut DISPLAY: Display = ();
-
-    #[init(spawn = [periodic])]
+    #[init]
     fn init() {
-        hprintln!("init").unwrap();
+        hprintln!("init...").unwrap();
 
         // Allocator
         {
@@ -72,65 +40,25 @@ const APP: () = {
         }
 
         let device: device::Peripherals = device;
+        let p0 = device.P0.split();
 
-        // Setup ADC1 on PA0
-        let adc1 = {
-            // Enable ADC clocks
-            device.RCC.apb2enr.write(|w| w.adc1en().set_bit());
-            // Power on ADC
-            device.ADC1.cr2.write(|w| w.adon().set_bit());
-
-            device.ADC1
-        };
-
-        let mut flash = device.FLASH.constrain();
-        let mut rcc = device.RCC.constrain();
-
-        // Try a different clock configuration
-        let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-        let mut afio = device.AFIO.constrain(&mut rcc.apb2);
-
-        let mut gpioa = device.GPIOA.split(&mut rcc.apb2);
-        let mut gpiob = device.GPIOB.split(&mut rcc.apb2);
-        let mut gpioc = device.GPIOC.split(&mut rcc.apb2);
-
-        // Radio being emulated by connecting USART1s of both controller and receiver together
-        let radio = {
-            let tx = gpiob.pb6.into_alternate_push_pull(&mut gpiob.crl);
-            let rx = gpiob.pb7;
-
-            let serial = Serial::usart1(
-                device.USART1,
-                (tx, rx),
-                &mut afio.mapr,
-                115_200.bps(),
-                clocks,
-                &mut rcc.apb2,
-            );
-
-            serial.split()
-        };
-
-        // SSD1306 connected to SPI2
         let display = {
-            let sck = gpiob.pb13.into_alternate_push_pull(&mut gpiob.crh);
-            let miso = gpiob.pb14;
-            let mosi = gpiob.pb15.into_alternate_push_pull(&mut gpiob.crh);
-            let mut rst = gpioa.pa8.into_push_pull_output(&mut gpioa.crh);
-            let dc = gpioa.pa9.into_push_pull_output(&mut gpioa.crh);
+            let spi = {
+                let sck = p0.p0_25.into_push_pull_output(Level::Low).degrade();
+                let miso = p0.p0_24.into_floating_input().degrade();
+                let mosi = p0.p0_23.into_push_pull_output(Level::Low).degrade();
 
-            let spi = Spi::spi2(
-                device.SPI2,
-                (sck, miso, mosi),
-                Mode {
-                    polarity: Polarity::IdleLow,
-                    phase: Phase::CaptureOnFirstTransition,
-                },
-                8.mhz(),
-                clocks,
-                &mut rcc.apb1,
-            );
+                let pins = hal::spim::Pins {
+                    sck,
+                    miso: Some(miso),
+                    mosi: Some(mosi),
+                };
+
+                device.SPIM0.constrain(pins)
+            };
+
+            let dc = p0.p0_09.into_push_pull_output(Level::Low).degrade();
+            let mut rst = p0.p0_02.into_push_pull_output(Level::Low).degrade();
 
             let mut display: GraphicsMode<_> = Builder::new()
                 .with_size(DisplaySize::Display128x64)
@@ -148,53 +76,30 @@ const APP: () = {
             display
         };
 
-        let led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+        hprintln!("init complete\n").unwrap();
 
-        spawn.periodic().unwrap();
-
-        hprintln!("init complete").unwrap();
-
-        LED = led;
-        ADC1 = adc1;
-        RADIO = radio;
+        COUNT = 0;
         DISPLAY = display;
     }
 
-    #[task(schedule = [periodic], resources = [LED, DISPLAY, RADIO, ADC1])]
-    fn periodic() {
-        let request = {
-            resources.ADC1.cr2.write(|w| w.adon().set_bit());
-            while resources.ADC1.sr.read().eoc().bit_is_clear() {}
-            let val = resources.ADC1.dr.read().data().bits();
+    #[idle(resources = [COUNT, DISPLAY])]
+    fn idle() -> ! {
+        loop {
+            *resources.COUNT += 1;
 
-            Request { val }
-        };
+            resources.DISPLAY.draw(
+                Font6x8::render_str(&format!("count: {}", resources.COUNT))
+                    .translate(Coord::new(0, 0))
+                    .into_iter(),
+            );
+            resources.DISPLAY.flush().unwrap();
 
-        let mut buf = [0u8; 32];
-        let len = ssmarshal::serialize(&mut buf, &request).unwrap();
-
-        for i in 0..len {
-            block!(resources.RADIO.0.write(buf[i])).unwrap();
+            //hprintln!("idle, count: {}", resources.COUNT).unwrap();
         }
-        block!(resources.RADIO.0.flush()).unwrap();
-
-        resources.DISPLAY.draw(
-            Font6x8::render_str(&format!("{:?}", request))
-                .translate(Coord::new(0, 0))
-                .into_iter(),
-        );
-        resources.DISPLAY.draw(
-            Font6x8::render_str(&format!("{:?}", buf[..len].to_vec()))
-                .translate(Coord::new(0, 12))
-                .into_iter(),
-        );
-        resources.DISPLAY.flush().unwrap();
-
-        schedule.periodic(scheduled + PERIOD.cycles()).unwrap();
     }
 
     extern "C" {
-        fn TAMPER();
+        fn PDM();
     }
 };
 
--- receiver/src/main.rs
+++ receiver/src/main.rs
@@ -3,87 +3,37 @@
 
 extern crate panic_semihosting;
 
-use common::Request;
-use cortex_m::singleton;
 use cortex_m_semihosting::hprintln;
-use nb::block;
 use rtfm::app;
-use stm32f103xx_hal::{
-    gpio::{gpioc::PC13, Output, PushPull},
-    prelude::*,
-    serial::Serial,
-    stm32f103xx::{self as device, USART1},
-};
 
-const PERIOD: u32 = 8_000_000;
+const PERIOD: u32 = 1000;
 
-#[app(device = stm32f103xx_hal::stm32f103xx)]
+#[app(device = nrf52810_pac)]
 const APP: () = {
-    static mut ON: bool = false;
-    static mut LED: PC13<Output<PushPull>> = ();
-    static mut C5: stm32f103xx_hal::dma::dma1::C5 = ();
-    static mut RADIO: (
-        stm32f103xx_hal::serial::Tx<USART1>,
-        stm32f103xx_hal::serial::Rx<USART1>,
-    ) = ();
-
-    #[init(spawn = [blinky])]
+    #[init]
     fn init() {
         hprintln!("init").unwrap();
-
-        let device: device::Peripherals = device;
-
-        let mut flash = device.FLASH.constrain();
-        let mut rcc = device.RCC.constrain();
-
-        // Try a different clock configuration
-        let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-        let mut afio = device.AFIO.constrain(&mut rcc.apb2);
-        let channels = device.DMA1.split(&mut rcc.ahb);
-        let c5 = channels.5;
-
-        let _gpioa = device.GPIOA.split(&mut rcc.apb2);
-        let mut gpiob = device.GPIOB.split(&mut rcc.apb2);
-        let mut gpioc = device.GPIOC.split(&mut rcc.apb2);
-
-        let radio = {
-            let tx = gpiob.pb6.into_alternate_push_pull(&mut gpiob.crl);
-            let rx = gpiob.pb7;
-
-            let serial = Serial::usart1(
-                device.USART1,
-                (tx, rx),
-                &mut afio.mapr,
-                115_200.bps(),
-                clocks,
-                &mut rcc.apb2,
-            );
-
-            serial.split()
-        };
-
-        let led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-
-        spawn.blinky().unwrap();
-
-        LED = led;
-        C5 = c5;
-        RADIO = radio;
+        // bootstrap the `periodic` task
+        //spawn.periodic().unwrap();
     }
 
-    #[task(schedule = [blinky], resources = [ON, LED])]
-    fn blinky() {
-        match *resources.ON {
-            true => resources.LED.set_high(),
-            false => resources.LED.set_low(),
+    #[idle]
+    fn idle() -> ! {
+        loop {
+            hprintln!("idle").unwrap();
         }
-        *resources.ON ^= true;
-
-        schedule.blinky(scheduled + PERIOD.cycles()).unwrap();
     }
+
+    /*
+    #[task(schedule = [periodic])]
+    fn periodic() {
+        hprintln!("periodic: scheduled:", scheduled).unwrap();
+
+        schedule.periodic(scheduled + PERIOD.cycles()).unwrap();
+    }
+    */
 
     extern "C" {
-        fn TAMPER();
+        fn PDM();
     }
 };
